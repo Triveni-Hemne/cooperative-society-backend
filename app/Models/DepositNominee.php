e0b603332f1ee3cb2d6029fc5c2495b1c45c0183
triveni <trivenihemne1999@gmail.com>
2025-02-15 10:22:57 +0530

    models

--- app/Models/DepositNominee.php
+++ app/Models/DepositNominee.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class DepositNominee extends Model
 {
     use HasFactory;
--- app/Models/Director.php
+++ app/Models/Director.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Director extends Model
 {
      use HasFactory;
