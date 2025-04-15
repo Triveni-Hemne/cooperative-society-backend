29f7278af54158b7a26df562d309fb28414b3e88
triveni <trivenihemne1999@gmail.com>
2025-04-15 11:03:47 +0530

    added branch_id field to the members and users table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'role' => Arr::random(['Director', 'Admin', 'Employee', 'Agent']),
+            'role' => Arr::random(['Director', 'Admin', 'Employee', 'Agent', 'Branch Manager']),
             'status' => Arr::random(['Active', 'Inactive']),
         ];
     }
